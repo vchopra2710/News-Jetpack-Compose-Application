3d603d9ed07373480d2eeb23cfa9e17462b1e7dc
vchopra2710 <vchopra.2710@gmail.com>
2021-01-12 11:30:32 +0100

    data class for artice cache

---
+++ app/src/main/java/com/proj/news/db/model/ArticleCache.kt
@@ -0,0 +1,19 @@
+package com.proj.news.db.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "articles")
+data class ArticleCache(
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "articleId") val articleId: Long = 0L,
+    @ColumnInfo(name = "author") val author: String?,
+    @ColumnInfo(name = "content") val content: String?,
+    @ColumnInfo(name = "description") val description: String?,
+    @ColumnInfo(name = "publishedAt") val publishedAt: String?,
+    @ColumnInfo(name = "title") val title: String?,
+    @ColumnInfo(name = "url") val url: String?,
+    @ColumnInfo(name = "urlToImage") val urlToImage: String?,
+    @ColumnInfo(name = "sourceId") val sourceId: String?,
+    @ColumnInfo(name = "sourceName") val sourceName: String?
+)

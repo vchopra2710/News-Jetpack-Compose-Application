cff7b32c612e561ef2d387701879a7ea0786a3a2
vchopra2710 <vchopra.2710@gmail.com>
2021-01-12 11:01:35 +0100

    base application: to add hilt di and init timber

---
+++ app/src/main/java/com/proj/news/presentation/BaseApplication.kt
@@ -0,0 +1,24 @@
+package com.proj.news.presentation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import timber.log.Timber
+
+@HiltAndroidApp
+class BaseApplication : Application() {
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+
+    override fun onCreate() {
+        super.onCreate()
+        delayedInit()
+    }
+
+    private fun delayedInit() {
+        applicationScope.launch {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}

a6887f0486040d8941c8df9c4adcb2f93b3b68d0
vchopra2710 <vchopra.2710@gmail.com>
2021-01-12 11:29:32 +0100

    news dao: queries to insert and fetch data from  database

---
+++ app/src/main/java/com/proj/news/db/NewsDao.kt
@@ -0,0 +1,22 @@
+package com.proj.news.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.proj.news.db.model.ArticleCache
+import com.proj.news.domain.model.Article
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NewsDao {
+    @JvmSuppressWildcards
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertArticles(articles: List<ArticleCache>?)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertArticle(article: ArticleCache?): Long
+
+    @Query("SELECT * FROM articles")
+    fun getArticles(): Flow<List<Article>>
+}

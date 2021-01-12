1318bb25af2fae1b2d8537a19a555f1ee2f89fea
vchopra2710 <vchopra.2710@gmail.com>
2021-01-12 11:38:28 +0100

    api key added to constants

--- app/src/main/java/com/proj/news/util/Constants.kt
+++ app/src/main/java/com/proj/news/util/Constants.kt
@@ -4,6 +4,7 @@ const val DBG_TAG = "ON-MONEY"
 
 // retrofit
 const val BASE_URL = "https://newsapi.org/"
+const val API_KEY = "YOUR_KEY"
 const val DEFAULT_SEARCH_COUNTRY = "in"
 const val READ_TIMEOUT: Long = 100
 const val CONNECT_TIMEOUT: Long = 100

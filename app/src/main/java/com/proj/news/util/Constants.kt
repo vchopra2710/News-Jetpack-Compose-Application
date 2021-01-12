b0e0454a753391962e8fe64a136a53508f1e1dfb
vchopra2710 <vchopra.2710@gmail.com>
2021-01-12 11:22:33 +0100

    constants for retrofit (base url, time out time and search country)

--- app/src/main/java/com/proj/news/util/Constants.kt
+++ app/src/main/java/com/proj/news/util/Constants.kt
@@ -1,3 +1,9 @@
 package com.proj.news.util
 
 const val DBG_TAG = "ON-MONEY"
+
+// retrofit
+const val BASE_URL = "https://newsapi.org/"
+const val DEFAULT_SEARCH_COUNTRY = "in"
+const val READ_TIMEOUT: Long = 100
+const val CONNECT_TIMEOUT: Long = 100

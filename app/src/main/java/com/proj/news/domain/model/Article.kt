d37c3913afd31e27f9668f4ff8562e9baefeea0f
vchopra2710 <vchopra.2710@gmail.com>
2021-01-12 11:17:04 +0100

    data class of an article for domain model

---
+++ app/src/main/java/com/proj/news/domain/model/Article.kt
@@ -0,0 +1,13 @@
+package com.proj.news.domain.model
+
+data class Article(
+    val author: String?,
+    val content: String?,
+    val description: String?,
+    val publishedAt: String?,
+    val title: String?,
+    val url: String?,
+    val urlToImage: String?,
+    val sourceId: String?,
+    val sourceName: String?
+)

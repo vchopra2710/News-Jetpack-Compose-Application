0ab82ed4b27640cd56fee9563718be6cdcd6e7cc
vchopra2710 <vchopra.2710@gmail.com>
2021-01-13 11:43:57 +0100

    composable used to display list of articles

--- app/src/main/java/com/proj/news/presentation/fragment/TopHeadLinesFragment.kt
+++ app/src/main/java/com/proj/news/presentation/fragment/TopHeadLinesFragment.kt
@@ -1,63 +1,66 @@
 package com.proj.news.presentation.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import androidx.fragment.app.Fragment
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import com.proj.news.R
-import com.proj.news.databinding.FragmentTopHeadLinesBinding
-import com.proj.news.events.DataStateEvent
-import com.proj.news.events.DataStateEvent.Loading
-import com.proj.news.events.DataStateEvent.Error
-import com.proj.news.events.DataStateEvent.Success
+import com.proj.news.presentation.components.ArticleCard
 import com.proj.news.util.DBG_TAG
 import com.proj.news.viewmodel.TopHeadLinesViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 
 @AndroidEntryPoint
-class TopHeadLinesFragment : Fragment(R.layout.fragment_top_head_lines) {
+class TopHeadLinesFragment : Fragment() {
 
     private val viewModel: TopHeadLinesViewModel by viewModels()
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Scaffold() {
+                    val articles = viewModel.articles.value
+                    val loading = viewModel.loading.value
 
-        val binding: FragmentTopHeadLinesBinding =
-            DataBindingUtil.setContentView(requireActivity(), R.layout.fragment_top_head_lines)
-        observeApi()
-        observeDatabase()
-    }
+                    Spacer(modifier = Modifier.padding(5.dp))
 
-    private fun observeApi() {
-        viewModel.dataStateRequestTopHeadlines.observe(viewLifecycleOwner, Observer { event ->
-            when (event) {
-                is Loading -> {
-                    Timber.d("$DBG_TAG getting data from api")
-                }
-                is Success<Boolean> -> {
-                    val venueFetched: Boolean = event.data
-                    Timber.d("$DBG_TAG got data from api => ${venueFetched}")
-                }
-                is Error -> {
-                    Timber.d("$DBG_TAG error getting data from api: ${event.exception.toString()}")
-                    // TODO: update ui(error page)
+                    Box(
+                        modifier = Modifier.fillMaxSize()
+                            .background(MaterialTheme.colors.background)
+                    ) {
+
+                        /** listview for articles **/
+                        LazyColumn {
+                            if (loading) {
+                                Timber.d("$DBG_TAG loading: $loading")
+                            } else {
+                                itemsIndexed(items = articles) { index, article ->
+                                    ArticleCard(article = article)
+                                }
+                            }
+                        }
+
+                        //TODO(create error page)
+                    }
                 }
             }
-        })
-    }
-
-    private fun observeDatabase() {
-        viewModel.domainArticleBlog.observe(viewLifecycleOwner, Observer { articles ->
-            if (articles.isEmpty()) return@Observer
-            Timber.d("$DBG_TAG, size : ${articles.size}")
-            /*for (article in articles) {
-                Timber.d("$DBG_TAG, ${articles.indexOf(article)} => ${article.title}")
-            }*/
-        })
+        }
     }
 }

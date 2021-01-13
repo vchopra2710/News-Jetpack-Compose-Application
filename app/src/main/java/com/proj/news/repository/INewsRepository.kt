18c29558e279e0beabc24942cf86e806899a5d27
vchopra2710 <vchopra.2710@gmail.com>
2021-01-13 11:45:36 +0100

    fun updated and one removed

--- app/src/main/java/com/proj/news/repository/INewsRepository.kt
+++ app/src/main/java/com/proj/news/repository/INewsRepository.kt
@@ -7,8 +7,5 @@ import kotlinx.coroutines.flow.Flow
 interface INewsRepository {
 
     // get top headlines from api
-    suspend fun fetchTopHeadLines(country: String?): Flow<DataStateEvent<Boolean>>
-
-    // get top head lines from db
-    fun getTopHeadLinesFromDB(): Flow<List<Article>>
+    suspend fun fetchTopHeadLines(country: String?): List<Article>
 }

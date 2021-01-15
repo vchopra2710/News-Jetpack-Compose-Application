8be1a48b756c66479b983986016daf9de100ac50
vchopra2710 <vchopra.2710@gmail.com>
2021-01-16 00:58:19 +0100

    constant val change

--- app/src/main/java/com/proj/news/util/Constants.kt
+++ app/src/main/java/com/proj/news/util/Constants.kt
@@ -5,6 +5,6 @@ const val DBG_TAG = "ON-MONEY"
 // retrofit
 const val BASE_URL = "https://newsapi.org/"
 const val API_KEY = "YOUR_KEY"
-const val DEFAULT_SEARCH_COUNTRY = "in"
+const val DEFAULT_SEARCH_COUNTRY_ALPHA_CODE = "in"
 const val READ_TIMEOUT: Long = 100
 const val CONNECT_TIMEOUT: Long = 100

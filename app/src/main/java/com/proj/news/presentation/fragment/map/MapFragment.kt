d2b1d808aa0a0efe30562092af9de7ef7c4305b3
vchopra2710 <vchopra.2710@gmail.com>
2021-01-16 01:04:51 +0100

    map fragment

--- app/src/main/java/com/proj/news/presentation/fragment/map/MapFragment.kt
+++ app/src/main/java/com/proj/news/presentation/fragment/map/MapFragment.kt
@@ -2,20 +2,59 @@ package com.proj.news.presentation.fragment.map
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import com.proj.news.R
-import com.proj.news.util.DBG_TAG
+import com.proj.news.domain.model.Country
+import com.proj.news.util.buildCountryList
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 
 
 @AndroidEntryPoint
-class MapFragment : Fragment(R.layout.fragment_map) {
+class MapFragment : Fragment(R.layout.fragment_map), OnMapReadyCallback,
+    GoogleMap.OnMarkerClickListener {
+
+    private lateinit var countries: List<Country>
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        Timber.d("$DBG_TAG oncreate")
+        countries = buildCountryList()
+        (childFragmentManager.findFragmentById(R.id.map_view) as SupportMapFragment).getMapAsync(
+            this
+        )
+    }
+
+    override fun onMapReady(p0: GoogleMap?) {
+        p0?.setOnMarkerClickListener(this)
+        addMultipleMarker(p0)
+    }
+
+    override fun onMarkerClick(p0: Marker?): Boolean {
+        val bundle = bundleOf(
+            requireContext().getString(R.string.country_name_tag) to p0?.title,
+            requireContext().getString(R.string.country_alpha_code_tag) to p0?.tag
+        )
+        findNavController().navigate(R.id.action_mapFragment_to_articleListFragment, bundle)
+        return true
+    }
+
+    private fun addMultipleMarker(mMap: GoogleMap?) {
+        for (country in countries) {
+            mMap?.addMarker(
+                MarkerOptions().position(country.latLng).title(country.name)
+            )?.tag = country.alpha2Code
+
+            if (country.focused) {
+                mMap?.moveCamera(CameraUpdateFactory.newLatLng(country.latLng))
+            }
+        }
     }
 }

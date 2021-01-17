de02a6ef84ec8ea052a08eeaf8ed5ff987d7434e
vchopra2710 <vchopra.2710@gmail.com>
2021-01-17 15:36:01 +0100

    convert network to cache and cache to domain

--- app/src/main/java/com/proj/news/repository/NewsRepositoryImpl.kt
+++ app/src/main/java/com/proj/news/repository/NewsRepositoryImpl.kt
@@ -25,12 +25,20 @@ constructor(
 
     override suspend fun fetchTopHeadLines(query: String?, country: String?): List<Article> {
         try {
+            // dto model from rest api
             val networkBlog: List<ArticleDto>? = apiClient.fetchTopHeadLines(
                 query = query, country = country
             ).articles
-            val domainBlog: List<Article> = articleDtoMapper.toDomainList(networkBlog)
-            val cacheDomain: List<ArticleCache> = articleCacheMapper.fromDomainList(domainBlog)
+
+            // cache model
+            val cacheDomain: List<ArticleCache> = articleDtoMapper.toCacheList(networkBlog)
+
+            // insert list to database
             newsDao.insertArticles(cacheDomain)
+
+            // domain blog
+            val domainBlog: List<Article> = articleCacheMapper.toDomainList(cacheDomain)
+
             return domainBlog
         } catch (e: Exception) {
             val exArticleDomainBlog = Article(

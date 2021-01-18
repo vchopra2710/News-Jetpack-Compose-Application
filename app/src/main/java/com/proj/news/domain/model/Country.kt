59f9eb31abf4fdb9054f39aae871b19051376300
vchopra2710 <vchopra.2710@gmail.com>
2021-01-18 09:46:08 +0100

    new parameter added for resource id

--- app/src/main/java/com/proj/news/domain/model/Country.kt
+++ app/src/main/java/com/proj/news/domain/model/Country.kt
@@ -1,10 +1,12 @@
 package com.proj.news.domain.model
 
+import androidx.annotation.DrawableRes
 import com.google.android.gms.maps.model.LatLng
 
 data class Country(
     val name: String,
     val latLng: LatLng,
     val alpha2Code: String,
-    val focused: Boolean = false
+    val focused: Boolean = false,
+    @DrawableRes val resId: Int?
 )

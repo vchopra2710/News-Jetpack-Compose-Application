504990cd2e33532558ef4cff5f2181f8788a3ac7
vchopra2710 <vchopra.2710@gmail.com>
2021-01-20 15:51:33 +0100

    main repository interface

--- app/src/main/java/com/proj/news/repository/IMainRepository.kt
+++ app/src/main/java/com/proj/news/repository/IMainRepository.kt
@@ -1,10 +1,8 @@
 package com.proj.news.repository
 
 import com.proj.news.domain.model.Article
-import com.proj.news.events.DataStateEvent
-import kotlinx.coroutines.flow.Flow
 
-interface INewsRepository {
+interface IMainRepository {
 
     // get top headlines from api
     suspend fun fetchTopHeadLines(query: String?, country: String?): List<Article>

92082bc97b0c101a8e808b730b5a7fcfa97b4e02
vchopra2710 <vchopra.2710@gmail.com>
2021-01-28 14:48:08 +0100

    map fragment cleaned(code moved to different classes)

--- app/src/main/java/com/proj/news/presentation/fragment/map/MapFragment.kt
+++ app/src/main/java/com/proj/news/presentation/fragment/map/MapFragment.kt
@@ -5,25 +5,18 @@ import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.viewinterop.AndroidViewBinding
-import androidx.core.os.bundleOf
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import com.google.android.gms.maps.*
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.MarkerOptions
-import com.proj.news.R
-import com.proj.news.databinding.FragmentMapBinding
 import com.proj.news.domain.model.Country
 import com.proj.news.util.buildCountryList
-import com.proj.news.util.getResBitmap
+import com.proj.news.viewmodel.MapViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class MapFragment : Fragment() {
-
+    private val viewModel: MapViewModel by viewModels()
     private lateinit var countries: List<Country>
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,66 +32,13 @@ class MapFragment : Fragment() {
     ): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                MaterialTheme {
-                    AndroidViewBinding(FragmentMapBinding::inflate) {
-
-                        // setting bundle:null oncreate map
-                        mapView.onCreate(null)
-
-                        // sync map
-                        mapView.getMapAsync { map ->
-
-                            // resume map
-                            mapView.onResume()
-
-                            // add markers of supported countries
-                            addMultipleMarker(map)
-
-                            // add marker onclick listener
-                            map?.setOnMarkerClickListener { marker ->
-
-                                marker?.let {
-
-                                    // add title and tag to bundle
-                                    val bundle = bundleOf(
-                                        requireContext().getString(R.string.country_name_tag) to it.title,
-                                        requireContext().getString(R.string.country_alpha_code_tag) to it.tag
-                                    )
-
-                                    // navigate to news fragment with bundle [{cntry_name=India, cntry_alpha_code=in}
-                                    findNavController().navigate(
-                                        R.id.action_mapFragment_to_articleListFragment,
-                                        bundle
-                                    )
-                                }
-
-                                false
-                            }
-                        }
-                    }
-                }
+                PopulateMapFragmentUI(
+                    nav = findNavController(),
+                    countries = countries,
+                    mapType = viewModel.mapType.value,
+                    changeMapType = viewModel::updateMapType
+                )
             }
-        }
-    }
-
-    private fun addMultipleMarker(mMap: GoogleMap?) {
-        // set map type type to normal
-        mMap?.mapType = GoogleMap.MAP_TYPE_NORMAL
-
-        // loop through countries
-        countries.forEach { country ->
-            // add marker to map
-            mMap?.addMarker(
-                MarkerOptions().position(country.latLng).title(country.name)
-                    .icon(
-                        BitmapDescriptorFactory.fromBitmap(
-                            getResBitmap(requireContext(), country.resId)
-                        )
-                    )
-            )?.tag = country.alpha2Code
-
-            // focus on map
-            if (country.focused) mMap?.moveCamera(CameraUpdateFactory.newLatLng(country.latLng))
         }
     }
 }
